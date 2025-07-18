1577bea98176743621c41249c88757705c78e250
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-07-18 13:18:44 +0530

    Update remove_duplicate_from_array.cpp

--- week3/26_Week3_Assignments/d_remove_duplicates_from_array/remove_duplicate_from_array.cpp
+++ week3/26_Week3_Assignments/d_remove_duplicates_from_array/remove_duplicate_from_array.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int removeDuplicatesFromArray(vector<int>arr){
+int countNumberOfUniqueElements(vector<int>arr){
     sort(arr.begin(),arr.end());
     int i=0,j=1;
     while(j<arr.size()){
@@ -20,9 +20,24 @@ int removeDuplicatesFromArray(vector<int>arr){
     return i+1;
 }
 
+vector<int>removeDuplicates(vector<int>nums){
+    vector<int>ans;
+    if(nums.empty()){
+        return ans;
+    }
+    sort(nums.begin(),nums.end());
+    ans.push_back(nums[0]);
+    for(int i = 1;i<nums.size();i++){
+        if(nums[i] != nums[i-1]){
+            ans.push_back(nums[i]);
+        }
+    }
+    return ans;
+}
+
 int main(){
     vector<int>arr = {0,1,2,3,0,4,5,3,2,1};
-    int ans = removeDuplicatesFromArray(arr);
+    int ans = countNumberOfUniqueElements(arr);
     cout<<"The number of Unique elements are : "<<ans<<" ";
     return 0;
 }

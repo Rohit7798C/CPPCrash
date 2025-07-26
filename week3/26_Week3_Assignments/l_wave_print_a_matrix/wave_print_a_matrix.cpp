ace387d16e43bb6729ccba1b1a69f6aa2b7ef5b2
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-07-26 10:46:57 +0530

    Update wave_print_a_matrix.cpp

--- week3/26_Week3_Assignments/l_wave_print_a_matrix/wave_print_a_matrix.cpp
+++ week3/26_Week3_Assignments/l_wave_print_a_matrix/wave_print_a_matrix.cpp
@@ -25,16 +25,36 @@ void wavePrintOfMatrix(vector<vector<int>>arr){
 }
 
 int main(){
-    vector<vector<int>>arr(3,vector<int>(4));
-    // int n;
-    cout<<"Enter the elements : ";
-    // cin>>n;
 
-    for(int i=0;i<3;i++){
-        for(int j=0;j<4;j++){
+    //if we doesn't explicitly specify the no. of rows and no. of columns, then we have to use resize().
+    vector<vector<int>>arr;
+    int numRows, numCols; 
+    cout<<"Enter the number of rows : ";
+    cin>>numRows;
+    cout<<"Enter the number of cols : ";
+    cin>>numCols;
+    // Resize the outer vector to hold 'numRows' inner vectors
+    arr.resize(numRows); 
+    
+    cout<<"Enter the elements (" << numRows << " rows, " << numCols << " columns): " << endl;
+    for(int i = 0;i<numRows;i++){
+        // For each row, resize the inner vector to hold 'numCols' elements
+        arr[i].resize(numCols); 
+        for(int j = 0;j<numCols;j++){
             cin>>arr[i][j];
         }
     }
+
+    // vector<vector<int>>arr(3,vector<int>(4));
+    // int n;
+    // cout<<"Enter the elements : ";
+    // cin>>n;
+
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<4;j++){
+    //         cin>>arr[i][j];
+    //     }
+    // }
     cout<<"Wave form is : ";
     wavePrintOfMatrix(arr);
     return 0;

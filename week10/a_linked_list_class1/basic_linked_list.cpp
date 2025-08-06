633a631051557d7b4bc26d114af4d91d797665e8
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-08-06 23:06:02 +0530

    Update basic_linked_list.cpp

--- week10/a_linked_list_class1/basic_linked_list.cpp
+++ week10/a_linked_list_class1/basic_linked_list.cpp
@@ -21,6 +21,36 @@ void printValue(Node * head){
     cout<<"nullptr"<<endl;
 }
 
+Node * insertAtBegin(Node * head,int value){
+    Node * newNode = new Node(value);
+    newNode ->next = head;
+    return newNode;
+}
+
+void insertAtEnd(Node * head, int value){
+    Node * newNode = new Node(value);
+    
+    if(head == nullptr){
+        head = newNode;
+        return;
+    }
+    Node * current = head;
+    while(current ->next != nullptr){
+        current = current -> next;
+    }
+    current -> next = newNode;
+}
+
+Node * middleValue(Node * head){
+    Node * slow = head;
+    Node * fast = head;
+    while(fast != nullptr && fast->next != nullptr){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+    }
+    return slow;
+}
+
 int main(){
     Node * head = new Node (1);
     Node * second = new Node(2);
@@ -28,9 +58,16 @@ int main(){
     
     head -> next = second;
     second -> next =  third;
+
+    head = insertAtBegin(head,00);
+    insertAtEnd(head,50);
     
     cout<<"The created linked list is : ";
     printValue(head);
+    cout<<"The middle value is : ";
+    Node * mid = middleValue(head);
+    cout<<mid->data<<" "<<endl;
     return 0;
 
 }
+

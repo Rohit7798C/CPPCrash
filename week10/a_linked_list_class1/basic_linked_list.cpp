917aba6b43ae61bf389bfa153bcfa932f13b8525
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-08-11 14:55:28 +0530

    Update basic_linked_list.cpp

--- week10/a_linked_list_class1/basic_linked_list.cpp
+++ week10/a_linked_list_class1/basic_linked_list.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//Node Structure Creation
 struct Node{
     int data;
     Node * next;
@@ -12,6 +13,7 @@ struct Node{
     }
 };
 
+//Traversing the list and printing the data of each Node.
 void printValue(Node * head){
     Node * current = head;
     while(current != nullptr){
@@ -21,12 +23,14 @@ void printValue(Node * head){
     cout<<"nullptr"<<endl;
 }
 
+//Inserting a new Node with a given value at the beginning of the Linked List
 Node * insertAtBegin(Node * head,int value){
     Node * newNode = new Node(value);
     newNode ->next = head;
     return newNode;
 }
 
+//Inserting a new Node with a given value at the end of the Linked List
 void insertAtEnd(Node * head, int value){
     Node * newNode = new Node(value);
     
@@ -41,6 +45,7 @@ void insertAtEnd(Node * head, int value){
     current -> next = newNode;
 }
 
+//Finding the middle of the Linked List using slow and fast pointer 
 Node * middleValue(Node * head){
     Node * slow = head;
     Node * fast = head;
@@ -52,10 +57,12 @@ Node * middleValue(Node * head){
 }
 
 int main(){
+    //Manually creating a Linked list with three nodes.
     Node * head = new Node (1);
     Node * second = new Node(2);
     Node * third = new Node(3);
-    
+
+    //Linking all the Nodes together.
     head -> next = second;
     second -> next =  third;
 
@@ -70,4 +77,5 @@ int main(){
     return 0;
 
 }
+
 

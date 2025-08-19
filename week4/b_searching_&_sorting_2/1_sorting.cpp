42b05e7324a39168ddb6f5c28156e5e26261d93e
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-08-19 11:04:17 +0530

    Update 1_sorting.cpp

--- week4/b_searching_&_sorting_2/1_sorting.cpp
+++ week4/b_searching_&_sorting_2/1_sorting.cpp
@@ -17,3 +17,30 @@ int peakIndexInArrayUsingMaxElement(vector<int>&nums){
     }
     return maxIndex;
 } 
+
+                                                                 //                   Neither moving left or right, it is the peak element
+                                                                 //                                   end = mid
+//Time Complexity : O(logN) => Binary Search                     //                                      Peak
+// int peakIndexInMountainArray(vector<int>&arr){                   //                                       /\ 
+//     int n = arr.size();                                          //                                      /  \  
+//     int start = 0, end = n - 1;                                  //            A Side                   /    \                 B Side
+//     while(start<end){                                            // (always arr[mid] < arr[mid + 1])   /      \   (always arr[mid-1] > arr[mid]) 
+//         int mid = start + (end - start)/2;                       //     (Always move to right)        /        \      (Always move to left)  
+//         if(arr[mid]<arr[mid + 1]){                               //         start = mid + 1          /          \          end = mid - 1
+//             start = mid + 1;                                     //  (we are starting from here) -> /            \ <- (we are starting from here)
+//         }
+//         else{
+//             end = mid;
+//             //Yahan agar hum end = mid - 1 karenge toh hum peak value miss kar denge.
+//             //Kyunki agar humara mid pointer peak value par aa jata hai toh bhi hum (mid - 1) hi karenge.
+//             //Par mid pointer, peak value ke upar aane par humne kahin par bhi shift nahi karna chahiye, kyunki humein peak value mil chuki hai.
+//             //Isiliye hum yaha par end = mid, use kar rahe hai, taki saari value check ho.
+
+//             //Infinite loop condition
+//             //agar hum start = mid and end = mid karte hai, aur while ki condition while(start<=end) karte hai
+//             //toh yeh ek infinite loop mein jaa sakta hai.
+//             //Isiliye jab bhi hum aisa karte hai toh humne while(start<end) use karna chahiye, taki infinite loop ka problem na ho.
+//         }
+//     }
+//     return start;
+// }

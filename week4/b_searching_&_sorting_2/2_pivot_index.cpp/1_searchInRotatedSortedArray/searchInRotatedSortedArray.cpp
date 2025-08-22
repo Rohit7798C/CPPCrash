bc93b3733af78c6ef489968c67fd3e1ee5ab0efe
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-08-22 10:40:26 +0530

    Add files via upload

---
+++ week4/b_searching_&_sorting_2/2_pivot_index.cpp/1_searchInRotatedSortedArray/searchInRotatedSortedArray.cpp
@@ -0,0 +1,39 @@
+//Finding the pivot element from a Sorted and Rotated Array.
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int pivotIndexFromArray(vector<int>&arr){
+    int n = arr.size();
+    int start = 0, end = n-1;
+    int mid = start + (end - start)/2;
+    int ansIndex = -1;
+    while(start<=end){
+        if(arr[mid]>arr[mid + 1]){
+            return mid;
+        }
+        if(arr[mid]<arr[mid - 1]){
+            return mid - 1;
+        }
+        //Kab left or Kab right
+        //Hume yeh pata hai ki, yeh ek rotated array hai aur iska pehela element last se hi rotate ho kar aaya hai.
+        //(50|60|70|10|20|30|40), 50 rotate ho kar aage gaya hai.
+        //Toh isiliye 50 hamesha B part ke saare elements se bada rahega.
+        //Toh hum B part ke elements ko array ke first element 50 se compare karke pata kar sakte hai ki, humein left jana hai ki right.
+        if(arr[start] > arr[mid]){
+            end = mid - 1;
+        }
+        else{
+            //right
+            start = mid;
+        }
+    }
+}
+
+int main(){
+    
+    return 0;
+}
+
+

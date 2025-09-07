ab947d887a720bd31a857f52e103bdc4e6960346
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-09-07 21:21:26 +0530

    Update LL_basics.cpp

--- week10/b_linked_list_class2/1_LL_basics/LL_basics.cpp
+++ week10/b_linked_list_class2/1_LL_basics/LL_basics.cpp
@@ -39,22 +39,45 @@ class Node{
         this -> next = NULL;
     }
     //Whenever a new node is created, this above constructor gets called and the data is inserted as above.
+
+
+    //Creating the Destructor 
+    // ~Node(){
+    //     int value = this -> data;
+    //     //code for freeing the memory
+    //     if(this -> next != NULL){
+    //         delete next;
+    //         this-> next = NULL;
+    //     }
+    //     cout<<"Memory is Free for Node with Data "<<value<<endl;
+    // }
 };
 
 //Here reference(&) of head is taken because we dont't want to make a Copy and want to make changes to the Original Linked List only.
-void insertAtHead(Node * &head, int data){
+void insertAtHead(Node * &head, Node * &tail,  int data){
     //creating New Node
     Node * temp = new Node(data);
     //Ab iska jo next hai, woh NULL ko point kar raha hai,
     //Toh usko wahan se hata kar head ko point karwa do (As below).
+    if(head == NULL){
+        head = temp;
+        tail = temp;
+        return;
+    }
     temp -> next = head;
     head = temp;
 }
 
 //Yeh function hamesha Ending Node ke aage new node add karega
-void insertAtTail(Node * &tail, int data){
+void insertAtTail(Node * &tail, Node * &head, int data){
     //Tail -> It is a pointer of Node Type which will always indicate the Last Node.
     Node * temp = new Node(data);
+
+    if(head == NULL){
+        head = temp;
+        tail = temp;
+        return;
+    }
 
     //Since both head and tail are pointing the same node, then we will make the tail node to point to New temp node.
     tail -> next = temp;
@@ -75,7 +98,7 @@ void insertAtPosition(Node * &tail, Node * &head, int position, int d){
     //For inserting at the 1st position we can do this,
     //Insert At Start.
     if(position == 1){
-        insertAtHead(head,d);
+        insertAtHead(head,tail,d);
         return;
     }
 
@@ -91,16 +114,19 @@ void insertAtPosition(Node * &tail, Node * &head, int position, int d){
     int count = 1;
     
     //If we want to insert at nth node then we have to go till (n-1) node.
-    while(count<position - 1){
+    while(temp != NULL && count<position - 1){
         //moving step by step 
         temp = temp -> next;
         count++;
     }
-    
+    if(temp == NULL){
+        cout<<"Position is Out Of Bounds!"<<endl;
+        return;
+    }
     //But we don't know the length, so we can do is,
     //inserting at the last Position.
     if(temp -> next  == NULL){
-        insertAtTail(tail,d);
+        insertAtTail(tail,head,d);
         return;
     }
     //Creating a Node for d
@@ -109,7 +135,71 @@ void insertAtPosition(Node * &tail, Node * &head, int position, int d){
     temp -> next = nodeToInsert;
 }
 
+
+//Deletion w.r.t position in Linked List.
+void deleteNode(Node * &head,Node * &tail, int position){
+
+    //Check if the List is Empty
+    if(head == nullptr){
+        cout<<"The list is already Empty. Nothing to delete"<<endl;
+        return;
+    }
+    
+    //Deletion of Start Node
+    if(position == 1){
+        Node * temp = head;
+        if(head->next == NULL){
+            head = nullptr;
+            tail = nullptr;
+        }
+        else{
+            head = head -> next;   //Update head to the next node
+            //memory free start node(because, since we have created this head using constructor, we have free the memory by explicitly calling the destructor).
+            //because it was stored in the Heap Memory.
+        }
+        
+        //Humein temp ke next ko bhi NULL karna padega kyunki woh abhi bhi next wale node ko point kar raha hai.
+        temp -> next = NULL;   //(same for else part also).
+        
+        delete temp;           //Free memory of the old head node
+    }
+    else{
+        //Deleting any middle or Last Node.
+        Node * current = head;
+        Node * prev = NULL;
+        
+        int count = 1;
+        while(current != nullptr && count<position){
+            //Because prev is already pointing to NULL, so we are moving the prev to current.
+            //And current to next.
+            prev = current;
+            current = current -> next;
+            count++;
+        }
+        //If current is null, the position is out of bounds.
+        if(current == nullptr){
+            cout<<"Position is out of Bounds!"<<endl;
+            return;
+        }
+
+        //IMP statement for actual deletion.
+        prev -> next = current -> next;
+        //Humein current ke next ko bhi NULL karna padega kyunki woh abhi bhi next wale node ko point kar raha hai.
+        current -> next = NULL;
+        
+        //If we've deleted the last node, update the tail to the previous node
+        if(prev -> next == nullptr){
+            tail = prev;
+        }
+        delete current;
+    }
+}
+
 void print(Node * head){
+    if(head == NULL){
+        cout<<"Linked List is Empty!"<<endl;
+        return;
+    }
     Node * temp = head;
     while(temp != NULL){
         cout<<temp -> data<<" -> ";
@@ -136,19 +226,35 @@ int main(){
     // print(head);
 
     //Creating a Tail pointer (For inserting from End)
-    //Because if there is a single node in LL then both head and tail will point to the same node.
+    //Because, if there is a single node in LL then both head and tail will point to the same node.
     Node * tail = node1;
-    insertAtTail(tail,12);
+    insertAtTail(tail,head,12);
     print(head);
-    insertAtTail(tail,15);
+    insertAtTail(tail,head,15);
     print(head);
-    insertAtTail(tail,20);
+    insertAtTail(tail,head,20);
     print(head);
 
     //Inserting at Given position.
     insertAtPosition(tail, head, 5,24);
     print(head);
+    
+    
+    // deleteNode(head, 1);
+    // print(head);
 
+    // deleteNode(head, 2);
+    // print(head);
+
+    cout<<"Head -> "<<head -> data<<endl;
+    cout<<"Tail -> "<<tail -> data<<endl;
+
+    deleteNode(head,tail, 4);
+    print(head);
+    
+    // deleteNode(head, 5);
+    // print(head);
+    //When we are deleting the Last Node, the Tail-> data gives Garbage Value.(How to handle this?)
     cout<<"Head -> "<<head -> data<<endl;
     cout<<"Tail -> "<<tail -> data<<endl;
 

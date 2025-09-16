56d55adbd07d870f173e5f183eb5db107e213e95
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-09-16 21:24:55 +0530

    Update last_word_using_one_loop.cpp

--- week5/28_Char_Array_Strings_Class2_Problems/f_kth_word_from_string/last_word_using_one_loop.cpp
+++ week5/28_Char_Array_Strings_Class2_Problems/f_kth_word_from_string/last_word_using_one_loop.cpp
@@ -26,3 +26,30 @@ int main(){
     cout<<"Last word is : "<<findLastWord(sentence1);
     return 0;
 }
+
+
+
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// string lastWord(string sentence, int n){
+//     int len = sentence.length();
+//     string ans = "";
+//     for(int i = len;i>=0;i--){
+//         if(sentence[i] == ' '){
+//             return ans;
+//         }else{
+//             ans.insert(ans.begin(),sentence[i]);
+//         }
+//     }
+//     return ans;
+// }
+
+// int main(){
+//     string sentence = "Leetcode is very Easy to solve!";
+//     int n = 6;
+//     string ans = lastWord(sentence,n);
+//     cout<<"The "<<n<<"th word is : "<<ans<<endl;
+//     return 0;
+// }
